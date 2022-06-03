6eb8d20d5010aaf57f48c811cc1c50b8f7f3a4f5
shatrunjay4321 <65161427+shatrunjay4321@users.noreply.github.com>
2022-06-04 00:20:22 +0530

    Committing files

---
+++ CodeChef/C++14/TSORT/51524725.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int a[n], i;
+	for(i=0; i<n; i++)
+	{
+	    cin>>a[i];
+	}
+	sort(a, a+n);
+	for(i=0; i<n; i++)
+	{
+	    cout<<a[i]<<endl;
+	}
+	return 0;
+}
