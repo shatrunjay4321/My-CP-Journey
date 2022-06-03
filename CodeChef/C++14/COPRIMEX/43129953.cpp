9d0fd245dd4548ede00b4032a0f540f8333fbf04
shatrunjay4321 <65161427+shatrunjay4321@users.noreply.github.com>
2022-06-04 00:19:56 +0530

    Committing files

---
+++ CodeChef/C++14/COPRIMEX/43129953.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t, i, j, l, r;
+    cin>>t;
+    while(t--)
+    {
+        cin>>l>>r;
+        cout<<1000003<<endl;
+    }
+    return 0;
+}
