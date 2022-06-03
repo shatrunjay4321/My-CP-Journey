7b7595ac596662f08d25a64c32c84a2b4a4326eb
shatrunjay4321 <65161427+shatrunjay4321@users.noreply.github.com>
2022-06-04 00:19:18 +0530

    Committing files

---
+++ CodeChef/C++14/GRIDXOR/56280042.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+void solve()
+{
+    ll n, i, t;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        for(t=0; t<n; t++)
+        {
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
