5f99f7e98e63a42aa92a102fe3c73297872db24c
shatrunjay4321 <65161427+shatrunjay4321@users.noreply.github.com>
2022-06-04 00:20:11 +0530

    Committing files

---
+++ CodeChef/C++14/MANYSUMS/41838900.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t, l, r, i;
+    cin>>t;
+    while(t--)
+    {
+        cin>>l>>r;
+        cout<<2*(r-l) + 1<<endl;
+    }
+	return 0;
+}
